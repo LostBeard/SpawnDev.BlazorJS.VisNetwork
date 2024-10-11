5ee4bb38289a4171dd4ee332f956a91445b8a94d
Todd Tanner <todd@spawndev.com>
2024-10-11 12:45:19 -0400

    Version 1.5.0. Updated SpawnDev.BlazorJS version.

--- SpawnDev.BlazorJS.VisNetwork/Network.cs
+++ SpawnDev.BlazorJS.VisNetwork/Network.cs
@@ -8,32 +8,128 @@ namespace SpawnDev.BlazorJS.VisNetwork
     /// <summary>
     /// Network is a visualization to display networks and networks consisting of nodes and edges. The visualization is easy to use and supports custom shapes, styles, colors, sizes, images, and more. The network visualization works smooth on any modern browser for up to a few thousand nodes and edges. To handle a larger amount of nodes, Network has clustering support. Network uses HTML canvas for rendering.<br />
     /// https://visjs.github.io/vis-network/docs/network/<br />
-    /// https://visjs.github.io/vis-network/examples/network/events/interactionEvents.html
     /// </summary>
     public class Network : JSObject
     {
+        /// <summary>
+        /// Deserialization constructor
+        /// </summary>
+        /// <param name="_ref"></param>
         public Network(IJSInProcessObjectReference _ref) : base(_ref){ }
+        /// <summary>
+        /// Creates a new instance of vis.Network
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="data"></param>
+        /// <param name="options"></param>
         public Network(ElementReference container, NetworkData data, NetworkOptions options) : base(JS.New("vis.Network", container, data, options)){ }
-        public void Fit() => JSRef.CallVoid("fit");
-        public string GetNodeAt(NetworkPointerPoint pointer) => JSRef.Call<string>("getNodeAt", pointer);
-        public void SelectNodes(IEnumerable<string> nodeIds) => JSRef.CallVoid("selectNodes", nodeIds);
-        public HTMLElement Body => JSRef.Get<HTMLElement>("canvas");
-        public HTMLCanvasElement Canvas => JSRef.Get<HTMLCanvasElement>("canvas");
-        public JSEventCallback<NetworkEvent> OnClick { get => new JSEventCallback<NetworkEvent>(JSRef, "click", "on", "off"); set { } }
-        public JSEventCallback<NetworkEvent> OnDoubleClick { get => new JSEventCallback<NetworkEvent>(JSRef, "doubleClick", "on", "off"); set { } }
-        public JSEventCallback<NetworkContextEvent> OnContext { get => new JSEventCallback<NetworkContextEvent>(JSRef, "oncontext", "on", "off"); set { } }
-        public JSEventCallback<NetworkDragEvent> OnDragging { get => new JSEventCallback<NetworkDragEvent>(JSRef, "dragging", "on", "off"); set { } }
-        public JSEventCallback<NetworkDragEvent> OnDragStart { get => new JSEventCallback<NetworkDragEvent>(JSRef, "dragStart", "on", "off"); set { } }
-        public JSEventCallback<NetworkDragEvent> OnDragEnd { get => new JSEventCallback<NetworkDragEvent>(JSRef, "dragEnd", "on", "off"); set { } }
-        public JSEventCallback<NetworkFocusEvent> OnHoverNode { get => new JSEventCallback<NetworkFocusEvent>(JSRef, "hoverNode", "on", "off"); set { } }
-        public JSEventCallback<NetworkFocusEvent> OnBlurNode { get => new JSEventCallback<NetworkFocusEvent>(JSRef, "blurNode", "on", "off"); set { } }
-        public JSEventCallback<NetworkSelectEvent> OnSelect { get => new JSEventCallback<NetworkSelectEvent>(JSRef, "select", "on", "off"); set { } }
-        public JSEventCallback<NetworkSelectEvent> OnSelectNode { get => new JSEventCallback<NetworkSelectEvent>(JSRef, "selectNode", "on", "off"); set { } }
-        public JSEventCallback<NetworkSelectEvent> OnDeselectNode { get => new JSEventCallback<NetworkSelectEvent>(JSRef, "deselectNode", "on", "off"); set { } }
-        public JSEventCallback<NetworkSelectEvent> OnSelectEdge { get => new JSEventCallback<NetworkSelectEvent>(JSRef, "selectEdge", "on", "off"); set { } }
-        public JSEventCallback<NetworkSelectEvent> OnDeselectEdge { get => new JSEventCallback<NetworkSelectEvent>(JSRef, "deselectEdge", "on", "off"); set { } }
-        public JSEventCallback<NetworkZoomEvent> OnZoom { get => new JSEventCallback<NetworkZoomEvent>(JSRef, "zoom", "on", "off"); set { } }
-        public JSEventCallback<string> OnShowPopup { get => new JSEventCallback<string>(JSRef, "showPopup", "on", "off"); set { } }
-        public JSEventCallback OnHidePopup { get => new JSEventCallback(JSRef, "hidePopup", "on", "off"); set { } }
+        /// <summary>
+        /// Zooms out so all nodes fit on the canvas.
+        /// </summary>
+        public void Fit() => JSRef!.CallVoid("fit");
+        /// <summary>
+        /// Returns a nodeId or undefined. The DOM positions are expected to be in pixels from the top left corner of the canvas.
+        /// </summary>
+        /// <param name="pointer"></param>
+        /// <returns></returns>
+        public string? GetNodeAt(NetworkPointerPoint pointer) => JSRef!.Call<string?>("getNodeAt", pointer);
+        /// <summary>
+        /// Selects the nodes corresponding to the id's in the input array. If highlightEdges is true or undefined, the neighbouring edges will also be selected. This method unselects all other objects before selecting its own objects. Does not fire events.
+        /// </summary>
+        /// <param name="nodeIds"></param>
+        public void SelectNodes(IEnumerable<string> nodeIds) => JSRef!.CallVoid("selectNodes", nodeIds);
+        /// <summary>
+        /// Unselect all objects. Does not fire events.
+        /// </summary>
+        public void UnselectAll() => JSRef!.CallVoid("unselectAll");
+        /// <summary>
+        /// Returns the network div
+        /// </summary>
+        public HTMLElement Body => JSRef!.Get<HTMLElement>("body");
+        /// <summary>
+        /// Returns the network canvas
+        /// </summary>
+        public HTMLCanvasElement Canvas => JSRef!.Get<HTMLCanvasElement>("canvas");
+        /// <summary>
+        /// Fired when the user clicks the mouse or taps on a touchscreen device.
+        /// </summary>
+        public ActionEvent<NetworkEvent> OnClick { get => new ActionEvent<NetworkEvent>("click", On, Off); set { } }
+        /// <summary>
+        /// Fired when the user double clicks the mouse or double taps on a touchscreen device. Since a double click is in fact 2 clicks, 2 click events are fired, followed by a double click event. If you do not want to use the click events if a double click event is fired, just check the time between click events before processing them.
+        /// </summary>
+        public ActionEvent<NetworkEvent> OnDoubleClick { get => new ActionEvent<NetworkEvent>("doubleClick", On, Off); set { } }
+        /// <summary>
+        /// Fired when the user click on the canvas with the right mouse button. The right mouse button does not select by default. You can use the method getNodeAt to select the node if you want.
+        /// </summary>
+        public ActionEvent<NetworkContextEvent> OnContext { get => new ActionEvent<NetworkContextEvent>("oncontext", On, Off); set { } }
+        /// <summary>
+        /// Fired when dragging node(s) or the view.
+        /// </summary>
+        public ActionEvent<NetworkDragEvent> OnDragging { get => new ActionEvent<NetworkDragEvent>("dragging", On, Off); set { } }
+        /// <summary>
+        /// Fired when starting a drag.
+        /// </summary>
+        public ActionEvent<NetworkDragEvent> OnDragStart { get => new ActionEvent<NetworkDragEvent>("dragStart", On, Off); set { } }
+        /// <summary>
+        /// Fired when the drag has finished.
+        /// </summary>
+        public ActionEvent<NetworkDragEvent> OnDragEnd { get => new ActionEvent<NetworkDragEvent>("dragEnd", On, Off); set { } }
+        /// <summary>
+        /// Fired if the option interaction:{hover:true} is enabled and the mouse hovers over a node.
+        /// </summary>
+        public ActionEvent<NetworkFocusEvent> OnHoverNode { get => new ActionEvent<NetworkFocusEvent>("hoverNode", On, Off); set { } }
+        /// <summary>
+        /// Fired if the option interaction:{hover:true} is enabled and the mouse moved away from a node it was hovering over before.
+        /// </summary>
+        public ActionEvent<NetworkFocusEvent> OnBlurNode { get => new ActionEvent<NetworkFocusEvent>("blurNode", On, Off); set { } }
+        /// <summary>
+        /// Fired when the selection has changed by user action. This means a node or edge has been selected, added to the selection or deselected. All select events are only triggered on click and hold.
+        /// </summary>
+        public ActionEvent<NetworkSelectEvent> OnSelect { get => new ActionEvent<NetworkSelectEvent>("select", On, Off); set { } }
+        /// <summary>
+        /// Fired when a node has been selected by the user.
+        /// </summary>
+        public ActionEvent<NetworkSelectEvent> OnSelectNode { get => new ActionEvent<NetworkSelectEvent>("selectNode", On, Off); set { } }
+        /// <summary>
+        /// Fired when a node (or nodes) has (or have) been deselected by the user. The previous selection is the list of nodes and edges that were selected before the last user event. Passes an object with properties structured as
+        /// </summary>
+        public ActionEvent<NetworkSelectEvent> OnDeselectNode { get => new ActionEvent<NetworkSelectEvent>("deselectNode", On, Off); set { } }
+        /// <summary>
+        /// Fired when an edge has been selected by the user.
+        /// </summary>
+        public ActionEvent<NetworkSelectEvent> OnSelectEdge { get => new ActionEvent<NetworkSelectEvent>("selectEdge", On, Off); set { } }
+        /// <summary>
+        /// Fired when an edge (or edges) has (or have) been deselected by the user. The previous selection is the list of nodes and edges that were selected before the last user event.
+        /// </summary>
+        public ActionEvent<NetworkSelectEvent> OnDeselectEdge { get => new ActionEvent<NetworkSelectEvent>("deselectEdge", On, Off); set { } }
+        /// <summary>
+        /// Fired when the user zooms in or out. The properties tell you which direction the zoom is in. The scale is a number greater than 0, which is the same that you get with network.getScale(). When fired by clicking the zoom in or zoom out navigation buttons, the pointer property of the object passed will be null.
+        /// </summary>
+        public ActionEvent<NetworkZoomEvent> OnZoom { get => new ActionEvent<NetworkZoomEvent>("zoom", On, Off); set { } }
+        /// <summary>
+        /// Fired when the popup (tooltip) is shown.
+        /// </summary>
+        public ActionEvent<string> OnShowPopup { get => new ActionEvent<string>("showPopup", On, Off); set { } }
+        /// <summary>
+        /// Fired when the popup (tooltip) is hidden.
+        /// </summary>
+        public ActionEvent OnHidePopup { get => new ActionEvent("hidePopup", On, Off); set { } }
+        /// <summary>
+        /// Set an event listener. Depending on the type of event you get different parameters for the callback function. Look at the event section of the documentation for more information.
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <param name="callback"></param>
+        public void On(string eventName, Callback callback) => JSRef!.CallVoid("on", eventName, callback);/// <summary>
+        /// Remove an event listener. The function you supply has to be the exact same as the one you used in the on function. If no function is supplied, all listeners will be removed. Look at the event section of the documentation for more information.
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <param name="callback"></param>
+        public void Off(string eventName, Callback callback) => JSRef!.CallVoid("off", eventName, callback);
+        /// <summary>
+        /// Set an event listener only once. After it has taken place, the event listener will be removed. Depending on the type of event you get different parameters for the callback function. Look at the event section of the documentation for more information.
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <param name="callback"></param>
+        public void Once(string eventName, Callback callback) => JSRef!.CallVoid("once", eventName, callback);
     }
 }
--- SpawnDev.BlazorJS.VisNetwork/NetworkEvent.cs
+++ SpawnDev.BlazorJS.VisNetwork/NetworkEvent.cs
@@ -3,14 +3,39 @@ using SpawnDev.BlazorJS.JSObjects;
 
 namespace SpawnDev.BlazorJS.VisNetwork
 {
+    /// <summary>
+    /// A common network event object
+    /// </summary>
     public class NetworkEvent : JSObject
     {
+        /// <summary>
+        /// Deserialization constructor
+        /// </summary>
+        /// <param name="_ref"></param>
         public NetworkEvent(IJSInProcessObjectReference _ref) : base(_ref) { }
-        public List<string> Edges => JSRef.Get<List<string>>("edges");
-        public Event Event => JSRef.Get<Event>("event");
-        public T EventAs<T>() => JSRef.Get<T>("event");
-        public JSObject Items => JSRef.Get<JSObject>("items");
-        public List<string> Nodes => JSRef.Get<List<string>>("nodes");
-        public NetworkEventPointer Pointer => JSRef.Get<NetworkEventPointer>("pointer");
+        /// <summary>
+        /// Edges related to this event
+        /// </summary>
+        public List<string> Edges => JSRef!.Get<List<string>>("edges");
+        /// <summary>
+        /// Event
+        /// </summary>
+        public Event Event => JSRef!.Get<Event>("event");
+        /// <summary>
+        /// Return the event property as type T
+        /// </summary>
+        public T EventAs<T>() => JSRef!.Get<T>("event");
+        /// <summary>
+        /// Items related to this event
+        /// </summary>
+        public JSObject Items => JSRef!.Get<JSObject>("items");
+        /// <summary>
+        /// Nodes related to this event
+        /// </summary>
+        public List<string> Nodes => JSRef!.Get<List<string>>("nodes");
+        /// <summary>
+        /// Pointer information for this event
+        /// </summary>
+        public NetworkEventPointer Pointer => JSRef!.Get<NetworkEventPointer>("pointer");
     }
 }
